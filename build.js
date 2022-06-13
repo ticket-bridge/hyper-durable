cbef147ebcba91e5e5e1eb78eceb6994d89777e9
Travis Frank <travis.frank@placeexchange.com>
2022-06-13 17:10:58 +0200

    feat: Declaration file + cjs

--- build.js
+++ build.js
@@ -5,15 +5,23 @@ import { build } from "esbuild";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const commonOptions = {
+  bundle: true,
+  sourcemap: true,
+  target: "esnext",
+  entryPoints: [path.join(__dirname, process.env.ENV === "prod" ? "src" : "test", "index.ts")],
+  outdir: path.join(__dirname, "dist"),
+};
+
 try {
   await build({
-    bundle: true,
-    sourcemap: true,
+    ...commonOptions,
     format: "esm",
-    target: "esnext",
-    entryPoints: [path.join(__dirname, process.env.ENV === "prod" ? "src" : "test", "index.ts")],
-    outdir: path.join(__dirname, "dist"),
     outExtension: { ".js": ".mjs" },
+  });
+  await build({
+    ...commonOptions,
+    format: "cjs",
   });
 } catch {
   process.exitCode = 1;
---
+++ src/index.d.ts
@@ -0,0 +1,50 @@
+/// <reference types="@cloudflare/workers-types" />
+
+import { Router } from "itty-router";
+
+export interface HyperState extends DurableObjectState {
+  dirty: Set<string>;
+  initialized?: Promise<void>;
+  persisted: Set<string>;
+  tempKey: string;
+}
+
+export class HyperDurable<Env = unknown> {
+  readonly isProxy?: boolean;
+  readonly original?: any;
+  env: Env;
+  state: HyperState;
+  storage: DurableObjectStorage;
+  router: Router;
+
+  constructor(state: DurableObjectState, env: Env);
+}
+
+export class HyperNamespaceProxy<T extends HyperDurable<ENV>, ENV> {
+  namespace: DurableObjectNamespace;
+  ref: T;
+  newUniqueId: (_options?: DurableObjectNamespaceNewUniqueIdOptions) => DurableObjectId;
+  idFromName: (name: string) => DurableObjectId;
+  idFromString: (hexId: string) => DurableObjectId;
+
+  constructor(
+    namespace: DurableObjectNamespace,
+    ref: new (state: DurableObjectState, env: ENV) => T
+  );
+  get(id: DurableObjectId): DurableObjectStub & {
+      [Prop in keyof T]?:
+        T[Prop] extends Function
+        ? () => Promise<unknown>
+        : Promise<unknown>;
+    } & {
+      [Prop in keyof T as T[Prop] extends Function ? never : `set${Capitalize<string & Prop>}`]?:
+        (newValue: T[Prop]) => Promise<unknown>
+    };
+}
+
+export function proxyHyperDurables<DO extends HyperDurable<ENV>, ENV>(
+  env: ENV,
+  doBindings: { [key: string]: new (state: DurableObjectState, env: ENV) => DO }
+): {
+  [Prop in keyof typeof doBindings]?: HyperNamespaceProxy<DO, ENV>
+}
